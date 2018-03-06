90713715df97e99f86c048a37c4061bf0a6b51ff
Adam Moller <31048865+AdamMoller@users.noreply.github.com>
2018-03-06 17:00:33 -0500

    add components and grid

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import Controls from './components/Controls.js';
 const styles = theme => ({
   app: {
   },
+  flashcard: {
+  },
 });
 
 class App extends Component {
@@ -17,7 +19,14 @@ class App extends Component {
     const { classes } = this.props;
     return (
       <React.Fragment>
+      <Navigation/>
         <Grid container direction="column" align="center" spacing={40} className={classes.app}>
+          <Grid item xs={11} sm={9} md={7} lg={5} className={classes.flashcard}>
+            <Flashcard word="sample"/>
+          </Grid>
+          <Grid item xs={11} sm={9} md={7} lg={5} align="center" className={classes.controls}>
+            <Controls/>
+          </Grid>
         </Grid>
       </React.Fragment>
     );
