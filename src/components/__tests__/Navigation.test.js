cd81e6c14e62d27ea2f85804c2c651cefe059a42
Adam Moller <31048865+AdamMoller@users.noreply.github.com>
2018-03-08 16:47:13 -0500

    add menu button

--- src/components/__tests__/Navigation.test.js
+++ src/components/__tests__/Navigation.test.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import Navigation from '../Navigation';
 
 describe('<Navigation />', () => {
+  let component, openMenu;
+  beforeEach(() => {
+    openMenu = jest.fn();
+    component = (
+      <Navigation
+        clickMenuButton={openMenu}
+      />
+    );
+  });
 
   it('renders without crashing', () => {
       const div = document.createElement('div');
-      ReactDOM.render(<Navigation />, div);
+      ReactDOM.render(component, div);
   });
 
   it('should render the header', () => {
-    const shallowRender = shallow(<Navigation />);
+    const shallowRender = mount(component);
     expect(shallowRender.contains("Flashcards")).toEqual(true);
   });
 
